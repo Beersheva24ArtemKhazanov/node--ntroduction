0442467a890c7da703ecbbba8cd8cf90bb3c2104
Artem Khazanov <tulenka04@gmail.com>
2025-02-03 10:34:34 +0200

    synchronous filesystem

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import logger from "./logger.mjs"; 
 import fs from 'node:fs';
 
-// const data = fs.readFileSync('./index.mjs');
-// logger.debug(data);
+const data = fs.readFileSync('./index.mjs', 'utf8');
+console.log(data);
 fs.writeFileSync('./file.txt', ['kuku', 'kukureku', 'Hello World'].join('\n'));
